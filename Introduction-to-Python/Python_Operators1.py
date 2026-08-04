f4b4597101a66f8c85a444fda3420ca85956ddef
Prof. Shivank Pandey <ershivank2012@gmail.com>
2026-08-04 19:08:14 +0530

    Operators in Python Sample Programs
    These programs let you understand the different ways of operator use in Python and its overloading.

---
+++ Introduction-to-Python/Python_Operators1.py
@@ -0,0 +1,35 @@
+a = 7
+b = 2
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)  
+
+# assign 10 to a
+a = 10
+
+# assign 5 to b
+b = 5 
+
+# assign the sum of a and b to a
+a += b      # a = a + b
+
+print(a)
+print (a>b)
---
+++ Introduction-to-Python/Python_Operators2.py
@@ -0,0 +1,33 @@
+a = 5
+b = 2
+
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
+
+x1 = 5
+y1 = 5
+x2 = 'Hello'
+y2 = 'Hello'
+x3 = [1,2,3]
+y3 = [1,2,3]
+
+print(x1 is not y1)  # prints False
+
+print(x2 is y2)  # prints True
+
+print(x3 is y3)  # prints False
