f089767d248f3c1a1787734e8f899f7968d2d998
Prof. Shivank Pandey <ershivank2012@gmail.com>
2026-08-05 15:54:27 +0530

    Python Program
    To learn the input, output, and variable typecasting.

---
+++ Introduction-to-Python/LearnPython.py
@@ -0,0 +1,27 @@
+#PROGRAM TO LEARN BASIC OUTPUT INPUT 
+
+print("HELLO")
+
+name = input("Give your Name Please: ")
+print("Your Name is: ", name)
+
+age = int(input("Enter age: "))
+print ("Your Age is: ",age)
+
+'''city = "Bareilly"
+print (city)'''
+
+city="NEW DELHI"
+kfc_locations = 5
+
+print("City:", city, "KFC_LOCATION:",kfc_locations)
+
+i = input("Enter Number: ")
+print(type(i))
+
+j = int(input("Enter Number: "))
+print(type(j))
+
+k = float(input("Enter Number: "))
+print(type(k))
+
